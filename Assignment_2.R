ca4b41f5aa5073ad5d7ec7ef8610e3761de6702e
Abdur Rasheed <abdulrasheedfashola@gmail.com>
2026-03-15 10:44:55 +0100

    Addvance Plot Graphics

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,4 +1,4 @@
-
+getwd()
 # MATRIC NUMBER: 24/56EG152
 # Sample data (30 observations)
 
@@ -6,16 +6,22 @@ set.seed(123)
 
 score <- rnorm(30, mean = 70, sd = 8)
 
-score
+cat("Generated Sample Daa:\n")
+print(score)
 
 # Descriptive Statistics
+
+cat("\nDescriptive Statistics:\n")
+print(summary(score))
 summary(score)
-mean(score)
-sd(score)
+cat("\nMean:",mean(score))
+cat("\nStandard Deviation:",sd(score))
 
 
+# Visualization
 # 1. Check Normality using Histogram
 
+par(mfrow=c(2,2))
 hist(score,
      main = "Histogram of Sample Scores",
      xlab = "Scores",
@@ -23,8 +29,9 @@ hist(score,
      border = "black")
 
 # 2. Normality Test (Shapiro-Wilk Test)
-
-shapiro.test(score)
+cat("\n\nShapiro-Wilk Normality Test:\n")
+normality_test <- shapiro.test(score)
+print(normality_test)
 
 # Q-Q Plot for Normality
 qqnorm(score)
@@ -36,7 +43,7 @@ qqline(score, col = "red")
 boxplot(score,
         main = "Boxplot for Outlier Detection",
         col = "lightgreen")
-
+cat("\nOutlier Detection using IQR:\n")
 Q1 <- quantile(score, 0.25)
 Q3 <- quantile(score, 0.75)
 
@@ -45,8 +52,8 @@ IQR_value <- IQR(score)
 lower_bond <- Q1 - 1.5*IQR_value
 upper_bond <- Q3 + 1.5*IQR_value
 
-score[score < lower_bond | score > upper_bond]
-
+outliers <- score[score < lower_bond | score > upper_bond]
+print(outliers)
 # 4. Check Serial Correlation
 # (Only relevant if data is time series)
 
@@ -55,20 +62,28 @@ acf(score,
 
 # 5. Choose Appropriate Test
 
-if(shapiro.test(score)$p.value > 0.05){
-  cat("Data is normally distributed -> Parametric Test(t-test)\n")
-  t.test(score, mu = 70)
+cat("\nTest Selection Based on Normality:\n")
+
+if(normality_test$p.value > 0.05){
+  cat("Data is normally distributed\n")
+  cat("Using Parametric Test: One-Sample t-test\n")
+  result <- t.test(score, mu=70)
 } else{
-  cat("Data is NOT normally distributed -> Use Nonparametric Test (Wilconxon Test)\n")
-  wilcox.test(score, mu = 70)
+  cat("Data is NOT normally distributed\n")
+  cat("Using Nonparametric Test: Wilconxon Test\n")
+  
+  result <- wilcox.test(score, mu=70)
+  
 }
+
+print(result)
 
 #---- INTERPRETATION---
 # If p-value > 0.05 in shapiro test,
 # we fail to reject the null hypothesis,
-# meaning the data follow a normal distributioin.
+# meaning the data follow a normal distribution.
 # Therefore, the parametric t-test is appropriate.
 
 # If p-value <= 0.05, 
-# normality assumpiton is violated,
+# normality assumption is violated,
 # so the wilcoxon signed-rank test is used.
